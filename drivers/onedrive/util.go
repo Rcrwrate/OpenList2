afe01ddb7738b5d21ad2bb6cd636b030f70f4c1f
MadDogOwner <xiaoran@xrgzs.top>
2025-07-21 15:44:38 +0800

    feat(onedrive): move read logic inside retry block

--- drivers/onedrive/util.go
+++ drivers/onedrive/util.go
@@ -247,15 +247,15 @@ func (d *Onedrive) upBig(ctx context.Context, dstDir model.Obj, stream model.Fil
 		}
 		left := stream.GetSize() - finish
 		byteSize := min(left, DEFAULT)
-		utils.Log.Debugf("[Onedrive] upload range: %d-%d/%d", finish, finish+byteSize-1, stream.GetSize())
-		byteData := make([]byte, byteSize)
-		n, err := io.ReadFull(stream, byteData)
-		utils.Log.Debug(err, n)
-		if err != nil {
-			return err
-		}
 		err = retry.Do(
 			func() error {
+				utils.Log.Debugf("[Onedrive] upload range: %d-%d/%d", finish, finish+byteSize-1, stream.GetSize())
+				byteData := make([]byte, byteSize)
+				n, err := io.ReadFull(stream, byteData)
+				utils.Log.Debug(err, n)
+				if err != nil {
+					return err
+				}
 				req, err := http.NewRequestWithContext(ctx, http.MethodPut, uploadUrl,
 					driver.NewLimitedUploadStream(ctx, bytes.NewReader(byteData)))
 				if err != nil {
