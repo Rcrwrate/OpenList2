1a1e1169ad1d7df08cb4eb718ca9126be11be5e6
花月喵梦 <152958106+nekohy@users.noreply.github.com>
2025-07-28 21:34:17 +0800

    Update internal/offline_download/tool/download.go
    Co-authored-by: MadDogOwner <xiaoran@xrgzs.top>
    Signed-off-by: 花月喵梦 <152958106+nekohy@users.noreply.github.com>

--- internal/offline_download/tool/download.go
+++ internal/offline_download/tool/download.go
@@ -174,7 +174,7 @@ func (t *DownloadTask) Update() (bool, error) {
 
 func (t *DownloadTask) Transfer() error {
 	toolName := t.tool.Name()
-	if toolName == "115 Cloud" || toolName == "PikPak" || toolName == "Thunder" || toolName == "ThunderX" || toolName == "ThunderBrowser" {
+	if toolName == "115 Cloud" || toolName == "115 Open" || toolName == "PikPak" || toolName == "Thunder" || toolName == "ThunderX" || toolName == "ThunderBrowser" {
 		// 如果不是直接下载到目标路径，则进行转存
 		if t.TempDir != t.DstDirPath {
 			return transferObj(t.Ctx(), t.TempDir, t.DstDirPath, t.DeletePolicy)
