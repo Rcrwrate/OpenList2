cd4a37f00c113ff0a244dd87f811bb64249a3d36
Seven <53081179+Seven66677731@users.noreply.github.com>
2025-07-29 19:01:51 +0800

    feat(fs): use cache directly when cache exist

--- internal/op/fs.go
+++ internal/op/fs.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	stdpath "path"
 	"slices"
+	"strings"
 	"time"
 
 	"github.com/OpenListTeam/OpenList/v4/internal/driver"
@@ -259,16 +260,27 @@ func Link(ctx context.Context, storage driver.Driver, path string, args model.Li
 		file model.Obj
 		err  error
 	)
-	if g, ok := storage.(driver.GetObjInfo); ok {
-		file, err = g.GetObjInfo(ctx, path)
-		if err != nil {
-			return nil, nil, errors.WithMessage(err, "failed to fetch file")
+	// use cache directly
+	dir, name := stdpath.Split(stdpath.Join(storage.GetStorage().MountPath, path))
+	if cacheFiles, ok := listCache.Get(strings.TrimSuffix(dir, "/")); ok {
+		for _, f := range cacheFiles {
+			if f.GetName() == name {
+				file = model.UnwrapObj(f)
+				break
+			}
 		}
 	} else {
-		file, err = GetUnwrap(ctx, storage, path)
+		if g, ok := storage.(driver.GetObjInfo); ok {
+			file, err = g.GetObjInfo(ctx, path)
+		} else {
+			file, err = GetUnwrap(ctx, storage, path)
+		}
+	}
+	if file == nil {
 		if err != nil {
 			return nil, nil, errors.WithMessage(err, "failed to get file")
 		}
+		return nil, nil, errors.WithStack(errs.ObjectNotFound)
 	}
 	if file.IsDir() {
 		return nil, nil, errors.WithStack(errs.NotFile)
