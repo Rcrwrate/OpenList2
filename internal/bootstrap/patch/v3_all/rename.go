85797bb2ce4bdfe8de12c5daa4ee29e67dc19dd1
MadDogOwner <xiaoran@xrgzs.top>
2025-07-31 03:58:52 +0800

    chore(patch): improve logging

--- internal/bootstrap/patch/v3_all/rename.go
+++ internal/bootstrap/patch/v3_all/rename.go
@@ -9,23 +9,25 @@ import (
 func RenameAlistV3Driver() {
 	storages, _, err := db.GetStorages(1, -1)
 	if err != nil {
-		utils.Log.Errorf("failed to get storages: %s", err.Error())
+		utils.Log.Errorf("[RenameAlistV3Driver] failed to get storages: %s", err.Error())
 		return
 	}
 
 	updatedCount := 0
 	for _, s := range storages {
 		if s.Driver == "AList V3" {
-			utils.Log.Warnf("Rename storage [%d]%s from Alist V3 to OpenList", s.ID, s.MountPath)
+			utils.Log.Warnf("[RenameAlistV3Driver] rename storage [%d]%s from Alist V3 to OpenList", s.ID, s.MountPath)
 			s.Driver = "OpenList"
 			err = db.UpdateStorage(&s)
 			if err != nil {
-				utils.Log.Errorf("failed to update storage [%d]%s: %s", s.ID, s.MountPath, err.Error())
+				utils.Log.Errorf("[RenameAlistV3Driver] failed to update storage [%d]%s: %s", s.ID, s.MountPath, err.Error())
 			} else {
 				updatedCount++
 			}
 		}
 	}
 
-	utils.Log.Warnf("RenameAlistV3Driver: updated %d storages from Alist V3 to OpenList", updatedCount)
+	if updatedCount > 0 {
+		utils.Log.Infof("[RenameAlistV3Driver] updated %d storages from Alist V3 to OpenList", updatedCount)
+	}
 }
